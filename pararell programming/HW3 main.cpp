2f12afb68b4690b20c8cac42aae49e9f6372721f
daeun <5ekdmsdldl@gmail.com>
2024-04-16 14:09:32 +0900

    split jobs for threads

--- pararell programming/HW3 main.cpp
+++ pararell programming/HW3 main.cpp
@@ -1,17 +1,27 @@
 #include "iostream"
 #define SZ 16
+#define nthread 4
+
+void runThread(int pid, double *out, int N, int cnt) {
+  int part = N / nthread;
+  int iStart = (nthread - pid) * part - 1;
+  int iEnd = std::max(cnt, (nthread - (pid + 1)) * part);
+
+  // std::cout << cnt << " : " << iEnd << " ~ " << iStart << std::endl;
+  for (int i = iStart; i >= iEnd; i--) {
+    out[i] += out[i - cnt];
+  }
+}
 
 void func(double *out, double *in, int N) {
   for (int i = 0; i < N; i++) {
     out[i] = in[i];
   }
 
-  int cnt = 1;
-  while (cnt < N) {
-    for (int i = N - 1; i >= cnt; i--) {
-      out[i] += out[i - cnt];
+  for (int cnt = 1; cnt < N; cnt *= 2) {
+    for (int pid = 0; pid < nthread; pid++) {
+      runThread(pid, out, N, cnt);
     }
-    cnt *= 2;
   }
 }
 
@@ -20,9 +30,7 @@ int main() {
   double in[SZ] = {0};
   for (int i = 0; i < SZ; i++) {
     in[i] = rand() % 100;
-    // std::cout << in[i] << " ";
   }
-  // std::cout << std::endl;
   func(out, in, SZ);
 
   int result = 1;
@@ -31,8 +39,6 @@ int main() {
     sum += in[i];
     if (sum != out[i])
       result = 0;
-    // std::cout << i << " : " << in[i] << " " << sum << " " << out[i]  << " "
-    // << (sum != out[i]) << std::endl;
   }
   std::cout << "result : " << result << std::endl;
 }
