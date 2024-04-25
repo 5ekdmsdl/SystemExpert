5709a9bbeed3ca052887226529f283b34a9e41cd
daeun <5ekdmsdldl@gmail.com>
2024-04-25 16:53:36 +0900

    add vector with 512 gridDim, 32 blockDim

--- pararellprogramming/cuda-vecadd/vec_add.cu
+++ pararellprogramming/cuda-vecadd/vec_add.cu
@@ -17,42 +17,42 @@ __global__ void vec_add_kernel(const int *A, const int *B, int *C, int N) {
 }
 
 __global__ void kernel_add(const int* a, const int* b, int* c){
-  *c = *a + *b;
+  int globalIdx = blockIdx.x * blockDim.x + threadIdx.x;
+  c[globalIdx] = a[globalIdx] + b[globalIdx];
+  return;
 }
 
 int main() {
-  // int N = 16384;    // = 32 * 512
-  // int *A = (int *) malloc(N * sizeof(int));
-  // int *B = (int *) malloc(N * sizeof(int));
-  // int *C = (int *) malloc(N * sizeof(int));
-  // int *C_ans = (int *) malloc(N * sizeof(int));
+  int N = 16384;     // = 32 * 512
+  int blkSize = 32;  // thread groups execute the same kernel code together
+  int *A = (int *) malloc(N * sizeof(int));
+  int *B = (int *) malloc(N * sizeof(int));
+  int *C = (int *) malloc(N * sizeof(int));
+  int *C_ans = (int *) malloc(N * sizeof(int));
 
-  // for (int i = 0; i < N; i++) {
-  //   A[i] = rand() % 1000;
-  //   B[i] = rand() % 1000;
-  //   C_ans[i] = A[i] + B[i];
-  // }
+  for (int i = 0; i < N; i++) {
+    A[i] = rand() % 1000;
+    B[i] = rand() % 1000;
+    C_ans[i] = A[i] + B[i];
+  }
 
-  // TODO: Run vector addition on GPU
-  // Save the result in C
-  int a = 1, b = 2, c;
+  // device pointers for a,b,c
   int *d_a, *d_b, *d_c;
-  cudaMalloc(&d_a, sizeof(int));
-  cudaMalloc(&d_b, sizeof(int));
-  cudaMalloc(&d_c, sizeof(int));
+  cudaMalloc(&d_a, sizeof(int) * N);
+  cudaMalloc(&d_b, sizeof(int) * N);
+  cudaMalloc(&d_c, sizeof(int) * N);
+  
+  cudaMemcpy(d_a, A, sizeof(int) * N, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, B, sizeof(int) * N, cudaMemcpyHostToDevice);
+  int gridSize = N / blkSize; // = 512 -> collection of blocks
+  kernel_add<<<gridSize, blkSize>>>(d_a, d_b, d_c);
+  cudaMemcpy(C, d_c, sizeof(int) * N, cudaMemcpyDeviceToHost);
 
-  cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
-  kernel_add<<<1,1>>>(d_a, d_b, d_c);
-  cudaMemcpy(&c, d_c, sizeof(int), cudaMemcpyDeviceToHost);
-  printf("c : %d \n",c);
-
-
-  // for (int i = 0; i < N; i++) {
-  //   if (C[i] != C_ans[i]) {
-  //     printf("Result differ at %d: %d vs %d\n", i, C[i], C_ans[i]);
-  //   }
-  // }
+  for (int i = 0; i < N; i++) {
+    if (C[i] != C_ans[i]) {
+      printf("Result differ at %d: %d vs %d\n", i, C[i], C_ans[i]);
+    }
+  }
 
   printf("Validation done.\n");
 
