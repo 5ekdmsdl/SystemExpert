4ea0a07dd248cda8754f4ba6c3c020b060ab9c2c
daeun <5ekdmsdldl@gmail.com>
2024-05-02 16:28:59 +0900

    cuda info example init commit

---
+++ pararellprogramming/cuda-info/cuda_info.cu
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+#define CHECK_CUDA(call)                                                 \
+  do {                                                                   \
+    cudaError_t status_ = call;                                          \
+    if (status_ != cudaSuccess) {                                        \
+      fprintf(stderr, "CUDA error (%s:%d): %s:%s\n", __FILE__, __LINE__, \
+              cudaGetErrorName(status_), cudaGetErrorString(status_));   \
+      exit(EXIT_FAILURE);                                                \
+    }                                                                    \
+  } while (0)
+
+int main() {
+  // TODO
+  return 0;
+}
---
+++ pararellprogramming/cuda-info/run_cuda_info.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+srun -N 1 --partition samsung --exclusive ./cuda_info
