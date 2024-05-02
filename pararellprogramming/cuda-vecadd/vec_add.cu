0acc6a48018c8b0b1618d9cdc6a39fa71af9a656
daeun <5ekdmsdldl@gmail.com>
2024-05-02 16:43:00 +0900

    vec add example

--- pararellprogramming/cuda-vecadd/vec_add.cu
+++ pararellprogramming/cuda-vecadd/vec_add.cu
@@ -24,7 +24,7 @@ __global__ void kernel_add(const int* a, const int* b, int* c){
 
 int main() {
   int N = 16384;     // = 32 * 512
-  int blkSize = 32;  // thread groups execute the same kernel code together
+  // int blkSize = 32;  // thread groups execute the same kernel code together
   int *A = (int *) malloc(N * sizeof(int));
   int *B = (int *) malloc(N * sizeof(int));
   int *C = (int *) malloc(N * sizeof(int));
@@ -44,8 +44,11 @@ int main() {
   
   cudaMemcpy(d_a, A, sizeof(int) * N, cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, B, sizeof(int) * N, cudaMemcpyHostToDevice);
-  int gridSize = N / blkSize; // = 512 -> collection of blocks
-  kernel_add<<<gridSize, blkSize>>>(d_a, d_b, d_c);
+  int blkSize = 32;
+  dim3 blkDim(blkSize);
+  dim3 gridDim((N + blkSize - 1) / blkSize); // = 512 -> collection of blocks
+    
+  kernel_add<<<gridDim, blkDim>>>(d_a, d_b, d_c);
   cudaMemcpy(C, d_c, sizeof(int) * N, cudaMemcpyDeviceToHost);
 
   for (int i = 0; i < N; i++) {
